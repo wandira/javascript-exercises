a06688d0399afb59df758e9bf88cd0684c3173ba
wandiramac <wandira.milleona@styletheory.co>
2021-05-26 21:43:47 +0700

    finish leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(year%4==0){
 
+        if(year%100==0){ //if century years
+            if(year%400==0){
+                return true;
+            }
+            return false;
+        }
+
+        return true; //non century years-> just dividable by 4 means it is leap year
+    }
+
+    return false; // not dividable by 4 at all
 };
 
 module.exports = leapYears;
