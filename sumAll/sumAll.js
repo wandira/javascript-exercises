54581edc404d803abc12459a5219fa9ee26e07c6
wandiramac <wandira.milleona@styletheory.co>
2021-05-26 21:37:20 +0700

    finish sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    //isNaN coerces string into number, so cannot be used here
+    if(typeof start != "number"||typeof end != "number") return 'ERROR';
+    if(start<0||end<0) return 'ERROR';
+    if(start==end) return start;
 
+    let temp= 0;
+    if(start>end){
+        temp= start;
+        start= end;
+        end= temp;
+    }
+
+    let result=0;
+    for(;start<=end;start++){
+        result=result+start
+    }
+    return result;
 };
 
 module.exports = sumAll;
