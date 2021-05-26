9afee5c124bae33c8a63c33f8d49bfd1be000a02
wandiramac <wandira.milleona@styletheory.co>
2021-05-26 17:15:27 +0700

    finish repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str,loop) {
+    if(loop<0) return 'ERROR';
+    if(loop==0||str=='') return '';
+
+    let final = '';
+
+    while(loop>0){
+        final = final+str;
+        loop--;
+    }
+    return final;
 
 };
 
