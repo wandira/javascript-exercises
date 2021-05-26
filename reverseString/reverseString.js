7733e6073a05e8db07d84e2a688cf19ebd2b98fa
wandiramac <wandira.milleona@styletheory.co>
2021-05-26 17:19:29 +0700

    finish reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    if (str=='') return '';
 
+    let len = str.length-1;
+    let final = '';
+
+    while(len>-1) {
+        final = final + str[len];
+        len--;
+    }
+    return final;
 };
 
 module.exports = reverseString;
