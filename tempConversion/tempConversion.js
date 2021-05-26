af1f0ff7ed4e6df7af606d6c2f6e48d2554658bc
wandiramac <wandira.milleona@styletheory.co>
2021-05-26 21:55:24 +0700

    finish tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  const exactCelcius = (fahrenheit-32) / 9 * 5;
+  let roundedCelcius = Math.round(exactCelcius * 10) / 10;
+  return roundedCelcius;
 };
 
-const ctof = function() {
+const ctof = function(celcius) {
+  const exactFahrenheit = celcius / 5 * 9 + 32;
+  let roundedFahrenheit = Math.round(exactFahrenheit * 10) / 10;
+  return roundedFahrenheit;
 
 };
 
