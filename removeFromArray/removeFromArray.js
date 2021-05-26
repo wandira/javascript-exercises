fed1555da94bc9105f344953747964abc0b75947
wandiramac <wandira.milleona@styletheory.co>
2021-05-26 21:03:42 +0700

    finish removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+//https://levelup.gitconnected.com/how-to-write-function-with-n-number-of-parameters-in-javascript-a916de1be7a2s
+const removeFromArray = function(arr, ...args) {
+    
+    function haveNoMatch (entry) {
+        for(let i=0;i<args.length;i++){
+            if(entry===args[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    const result = arr.filter(haveNoMatch);
+    return result;
 
 };
 
